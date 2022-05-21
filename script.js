1d17cdad066831537b6d55da8d0049e79e7dc185
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-05-21 13:48:36 -0300

    Alterações na finalização

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const eventButton = document.getElementById("btn")
-eventButton.addEventListener("click", () => {
+
+const calcIMC = () => {
     const name = document.getElementById("name").value
     const height = document.getElementById("height").value
     const weight = document.getElementById("weight").value
     const displayResult = document.getElementById("result")
 
-    if (name != "" && altura != "" && peso != "") {
+    if (name === "" && height === "" && weight === "") {
+        displayResult.innerHTML = "Preencha todos os campos!!!"
+    } else {
         const result = (weight / (height * height)).toFixed(1)
         displayResult.innerHTML = `${name} seu IMC é ${result}`
-    } else {
-        displayResult.innerHTML = "Preencha todos os campos!!!"
     }
+}
 
-})
+
+eventButton.addEventListener("click", calcIMC)
+
 
 
